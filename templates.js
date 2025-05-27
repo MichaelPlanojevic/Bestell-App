41a253acb41c072c466f17726a6b494c993f6cdc
Michael Doll <dollmichael53@gmail.com>
2025-05-27 10:48:10 +0200

    new sort of the basket for better visual effects

--- templates.js
+++ templates.js
@@ -16,13 +16,16 @@ function getTemplateMenu(dish, i) {
 function getTemplateBasket(i) {
     let item = basket[i];
     return `
-    <div class="basket-item">
-        <span>${item.quantity}x ${item.name} - ${(item.price * item.quantity).toFixed(2)} €</span>
-        <div class="basket-controls">
-            <button class="basket-button" onclick="decreaseQuantity(${i})">-</button>
-            <button class="basket-button" onclick="increaseQuantity(${i})">+</button>
-            <button class="basket-button" onclick="removeFromBasket(${i})">🗑️</button>
-        </div>
+    <div class="basket-item-details">
+        <div class="basket-name">${item.name}</div>
+
+    <div class="basket-controls">
+        <button class="basket-button" onclick="decreaseQuantity(${i})">-</button>
+        <span class="basket-quantity">${item.quantity}</span>
+        <button class="basket-button" onclick="increaseQuantity(${i})">+</button>
+        <button class="basket-button" onclick="clearBasket(${i})">🗑️</button>
+    </div>
+         <div class="basket-price">${(item.price * item.quantity).toFixed(2)} €</div>
     </div>
     `;
 }
